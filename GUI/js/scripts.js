679420b58cb959388e568298ba6249a6651535ec
Oz <frost57@gmail.com>
2020-07-14 20:15:15 -0400

    signup page, some js

---
+++ GUI/js/scripts.js
@@ -0,0 +1,21 @@
+﻿function passPrompt() {
+	var email = prompt("Enter email", "");
+    if(/*TODO: email exists*/false) {
+    	/*sent email*/
+    } else {
+       alert("Email does not exist");
+    } 
+}
+
+function newAccount() {
+	
+	var username = prompt("Enter username");
+	var password;
+	if(/*TODO: username already exists*/ false) {
+		alert("username already exists");
+		return;
+	} else {
+		password = prompt("enter password")
+	}
+	
+}
