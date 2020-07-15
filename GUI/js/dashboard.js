33c5120afde3320a49c28b776c2c5fb9c6e3ddda
Oz <frost57@gmail.com>
2020-07-15 16:21:29 -0400

    worked on view jobs

---
+++ GUI/js/dashboard.js
@@ -0,0 +1,25 @@
+function viewJobs() {
+
+    var html = "";
+
+    for (i = 0; i < 5; i++) {   //TODO: number of posted jobs
+
+        var datePosted = "";
+        var category = "";
+        var description = "";
+        var openings = "";
+        var applications = "";
+
+        html += "    <div class = \"row justify-content-center\">\n" +
+            "        <div class = \"col-10 border\">\n" +
+            "            <p class=\"jobTitle\"><b>Job Title goes here</b></p> <br>\n" +
+            "            <p><b>Date Posted: </b>" + datePosted + "</p>\n" +
+            "            <p><b>Category: </b>" + category + "</p>\n" +
+            "            <p><b>Description: </b>" + description + "</p>\n" +
+            "            <p><b># Openings: </b>" + openings + "</p>\n" +
+            "            <p><b># Applications: </b>" + applications + "</p>\n" +
+            "        </div>\n" +
+            "    </div>";
+    }
+    document.getElementById("viewJobs").innerHTML = html;
+}
