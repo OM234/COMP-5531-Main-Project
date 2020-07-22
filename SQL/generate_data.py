4b2f94301b13dd7f7dc58b29532e11c2160a7ec9
Craig-ling <craigboucher9@gmail.com>
2020-07-22 12:36:29 -0400

    added lists for company names, passwords, emails, and usernames.

--- SQL/generate_data.py
+++ SQL/generate_data.py
@@ -30,8 +30,8 @@ with open('names.txt', 'r') as nameFile:
     for line in nameFile:
         firstNameList.append(line.strip())
 
-# for i in range(len(firstNameList)):
-#    print(firstNameList[i])
+#for i in range(len(firstNameList)):
+    #print(firstNameList[i])
 
 with open('lastnames.txt', 'r') as lastNameFile:
     for line in lastNameFile:
@@ -55,6 +55,65 @@ for k in range(100):
     ccList.append(ccNumber)
     ccNumber = ""
 
-# for i in range(len(ccList)):
+#for i in range(len(ccList)):
 #    print(ccList[i])
+
+# Emails
+
+emailList = []
+emailString = ""
+emailDomain = ['hotmail.com', 'gmail.ca', 'yahoo.com']
+
+for i in range(100):
+    emailString += firstNameList[i] + lastNameList[i] + str(r.randrange(10)) + str(r.randrange(10)) + "@" +\
+                   emailDomain[r.randrange(2)]
+    emailList.append(emailString)
+    emailString = ""
+
+#for each in emailList:
+#    print(each)
+
+# Usernames
+
+userNameList = []
+userString = ""
+
+for j in range(100):
+    userString += firstNameList[j] + "_" + lastNameList[j][0] + str(r.randrange(10)) + str(r.randrange(10))
+    userNameList.append(userString)
+    userString = ""
+
+#for each in userNameList:
+#    print(each)
+
+# Passwords
+
+nounList = []
+passwordList = []
+
+with open('nouns.txt', 'r') as nounFile:
+    for line in nounFile:
+        nounList.append(line.strip())
+
+for u in range(100):
+    passwordList.append(nounList[r.randrange(len(nounList))] + str(r.randrange(10)) + str(r.randrange(10)) + str(r.randrange(10)))
+
+#for each in passwordList:
+#   print(each)
+
+# Employer Names
+
+businessList = []
+employerList = []
+
+with open('business_names.txt', 'r') as businessFile:
+    for line in businessFile:
+        businessList.append(line.strip())
+
+for p in range(100):
+    employerList.append(businessList[r.randrange(len(businessList))])
+
+#for each in employerList:
+#    print(each)
+
 
